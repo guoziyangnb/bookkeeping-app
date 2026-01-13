48ef90fe2dd201fdfe600e15793da91bd55169ba
gzynb <2263831821@qq.com>
2026-01-13 16:43:43 +0800

    feat：H5写的防侧滑退出应用

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+// H5防侧滑退出应用
+import '@/utils/app-back-button.js'
 
 // 导入样式
 import '@/assets/styles/base.css'
